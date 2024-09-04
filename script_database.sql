fd9cef42e5bcf70998deb5bc1ad7482ba865cfd3
Kevin Rivera <kevinriveravergaray@gmail.com>
2024-09-03 22:00:28 -0500

    table empleados

--- script_database.sql
+++ script_database.sql
@@ -47,3 +47,37 @@ descripcion VARCHAR(500)
 GO
 -- Funcion para visualizar el detalle de las tablas
 EXEC SP_HELP clientes;
+
+--Sucursales
+CREATE TABLE sucursales(
+id INT PRIMARY KEY IDENTITY(1,1),
+nombre VARCHAR(255) UNIQUE NOT NULL,
+telefono VARCHAR(20) NOT NULL,
+direccion NVARCHAR(500) NOT NULL,
+gerente_id INT );
+
+GO
+
+--Metodos de pago
+CREATE TABLE metodos_pago(
+id INT PRIMARY KEY IDENTITY(1,1),
+nombre VARCHAR(255) UNIQUE NOT NULL,
+estado BIT NOT NULL,
+descripcion VARCHAR(500) NOT NULL);
+
+GO
+
+-- Empleados
+CREATE TABLE empleados(
+id INT PRIMARY KEY IDENTITY(1,1),
+persona_id INT NOT NULL,
+sucursal_id INT NOT NULL,
+supervisor_id INT NULL,
+fecha_ingreso DATE NOT NULL,
+CONSTRAINT fk_persona_natural_empleado FOREIGN KEY (persona_id) REFERENCES personas_naturales(id),
+FOREIGN KEY (sucursal_id) REFERENCES sucursales(id),
+FOREIGN KEY (supervisor_id) REFERENCES empleados(id)
+);
+Go
+
+EXEC SP_HELP empleados;
