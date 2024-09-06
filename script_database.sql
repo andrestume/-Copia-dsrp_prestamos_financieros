83328c4718d75453d3293c7aab66b6b0212164a5
Kevin Rivera <kevinriveravergaray@gmail.com>
2024-09-05 20:11:12 -0500

    Script complete

--- script_database.sql
+++ script_database.sql
@@ -81,3 +81,69 @@ FOREIGN KEY (supervisor_id) REFERENCES empleados(id)
 Go
 
 EXEC SP_HELP empleados;
+
+-- Pagos
+
+CREATE TABLE pagos (
+id INT PRIMARY KEY IDENTITY(1,1),
+metodo_pago_id INT NOT NULL,
+codigo_operacion VARCHAR(20) NOT NULL,
+monto_pagado MONEY NOT NULL,
+fecha_pago datetime DEFAULT GETDATE() NOT NULL,
+created_at datetime2 NOT NULL,
+updated_at datetime2 NULL,
+deleted_at datetime2 NULL,
+created_by int NOT NULL,
+updated_by int NULL,
+deleted_by int NULL,
+FOREIGN KEY (metodo_pago_id) REFERENCES metodos_pago(id)
+);
+GO
+
+
+
+-- Prestamos
+
+CREATE TABLE prestamos (
+	id INT PRIMARY KEY IDENTITY(1,1),
+	cliente_id INT NOT NULL,
+	sucursal_id INT NOT NULL,
+	tipo_prestamo_id INT NOT NULL,
+	oficial_credito_id INT NOT NULL,
+	estado_prestamo VARCHAR(55) NOT NULL,
+	monto MONEY NOT NULL,
+	tasa_interes DECIMAL(9,4) NOT NULL,
+	plazo_meses INT NOT NULL,
+	fecha_inicio DATETIME DEFAULT GETDATE() NOT NULL
+	FOREIGN KEY (cliente_id) REFERENCES clientes(id),
+	FOREIGN KEY (sucursal_id) REFERENCES sucursales(id),
+	FOREIGN KEY (tipo_prestamo_id) REFERENCES tipos_prestamo(id),
+	FOREIGN KEY (oficial_credito_id) REFERENCES empleados(id)
+	);
+
+	GO
+
+-- Cuotas
+CREATE TABLE cuotas (
+	id INT PRIMARY KEY IDENTITY(1,1),
+	prestamo_id INT NOT NULL,
+	numero_cuota INT NOT NULL,
+	monto_cuota MONEY NOT NULL,
+	estado VARCHAR(55)NOT NULL,
+	fecha_vencimiento DATETIME DEFAULT GETDATE() NOT NULL
+	FOREIGN KEY (prestamo_id) REFERENCES prestamos(id)
+	);
+
+-- detalles_cuotas_pagos
+CREATE TABLE detalles_cuotas_pagos (
+	id INT PRIMARY KEY IDENTITY(1,1),
+	cuota_id INT NOT NULL,
+	pago_id INT NOT NULL,
+	saldo_restante MONEY NOT NULL,
+	);
+
+ALTER TABLE detalles_cuotas_pagos
+ADD CONSTRAINT fk_detalle_cuotas_pagos_cuotas FOREIGN KEY (cuota_id) REFERENCES cuotas(id);
+
+ALTER TABLE detalles_cuotas_pagos
+ADD CONSTRAINT fk_detalle_cuotas_pagos_pagos FOREIGN KEY (pago_id) REFERENCES pagos(id);
