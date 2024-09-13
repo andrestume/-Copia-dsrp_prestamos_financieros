e2f420519c868b4f241b5fd26f87111cf09a2813
Kevin Rivera <kevinriveravergaray@gmail.com>
2024-09-12 20:49:13 -0500

    end data selection

--- Ejercicios 12-09-2024.sql
+++ Ejercicios 12-09-2024.sql
@@ -92,3 +92,123 @@ WHERE
 	YEAR(fecha_constitucion)>'2005' AND
 	(razon_social LIKE '%h%' OR
 	domicilio_fiscal like 'Av.%');
+
+-- ENCONTRAR FILAS QUE ESTEN EN UNA LISTA DE VALORES
+SELECT*FROM tipos_prestamo;
+-- DEVOLVER LOS DATOS DE LOS SIGUIENTES TIPOS DE PRESTAMO 
+--'Préstamo Personal','Préstamo Educativo','Línea de Crédito'
+SELECT 
+	id,
+	tipo_prestamo_id,
+	monto, 
+	tasa_interes,
+	plazo_meses 
+FROM prestamos
+WHERE tipo_prestamo_id IN (3,4,5);
+
+SELECT 
+	p.id AS 'prestamo_id',
+	p.tipo_prestamo_id,
+	tp.nombre AS 'tipo_prestamo',
+	p.monto, 
+	p.tasa_interes,
+	p.plazo_meses 
+FROM prestamos p, tipos_prestamo tp
+WHERE 
+	p.tipo_prestamo_id=tp.id AND 
+	tp.nombre IN ('Préstamo Personal','Préstamo Educativo','Línea de Crédito');
+
+-- DEVOLVER LOS DATOS QUE NO PERTENEZCAN A LOS SIGUIENTES TIPOS DE PRESTAMO 
+--'Préstamo Personal','Préstamo Educativo','Línea de Crédito'
+
+SELECT 
+	p.id AS 'prestamo_id',
+	p.tipo_prestamo_id,
+	tp.nombre AS 'tipo_prestamo',
+	p.monto, 
+	p.tasa_interes,
+	p.plazo_meses 
+FROM prestamos p, tipos_prestamo tp
+WHERE 
+	p.tipo_prestamo_id=tp.id AND 
+	tp.nombre NOT IN ('Préstamo Personal','Préstamo Educativo','Línea de Crédito');
+
+-- INTERVALO DE VALORES
+
+SELECT*FROM prestamos;
+
+-- Devolver los registros de prestamos cuyo plazo en meses este entre 20 y 25 meses.
+
+SELECT*FROM prestamos
+WHERE plazo_meses BETWEEN 20 AND 25;
+
+SELECT*FROM prestamos
+WHERE plazo_meses>=20 AND plazo_meses<=25;
+
+-- SUBCONSULTA
+-- DEVOLVER LOS DATOS de los prestamos cuyo estado se encuentre en estado "Vencido" o "En Mora"
+SELECT*FROM estados_prestamo;
+
+SELECT 
+	p.id AS 'prestamo_id',
+	p.tipo_prestamo_id,
+	p.estado_prestamo_id,
+	p.monto, 
+	p.tasa_interes,
+	p.plazo_meses 
+FROM prestamos p
+WHERE 
+ 	p.estado_prestamo_id IN (SELECT id FROM estados_prestamo WHERE nombre IN('Vencido','En Mora'));
+
+--- DEVOLVER ESTADISTICAS DE LOS PRESTAMOS EN ESTADO "VENCIDO" y en ESTADO "En Mora"
+
+SELECT 
+	estado_prestamo_id,
+	SUM(monto) AS 'monto_total',
+	AVG(monto) AS 'promedio',
+	MAX (monto) AS 'Maximo_monto_prestado',
+	MIN(monto) AS 'Minimo_monto_prestado'
+FROM prestamos
+WHERE 
+ estado_prestamo_id IN (SELECT id FROM estados_prestamo WHERE nombre IN('Vencido','En Mora'))
+ GROUP BY estado_prestamo_id;
+
+
+ -- LISTAR las estadisticas de los estados de prestamos cuyo monto toal supere los 5 millones de soles
+ SELECT 
+	estado_prestamo_id,
+	SUM(monto) AS 'monto_total',
+	ROUND(AVG(monto),2) AS 'promedio',
+	MAX (monto) AS 'Maximo_monto_prestado',
+	MIN(monto) AS 'Minimo_monto_prestado'
+FROM prestamos
+GROUP BY estado_prestamo_id
+HAVING SUM(monto)>'5000000'
+ORDER BY 2 DESC;
+ -- LISTAR las estadisticas de los estados de prestamos cuyo monto toal supere los 5 millones de soles
+ SELECT 
+	SUM(monto) AS 'monto_total',
+	ROUND(AVG(monto),2) AS 'promedio',
+	MAX (monto) AS 'Maximo_monto_prestado',
+	MIN(monto) AS 'Minimo_monto_prestado'
+FROM prestamos
+HAVING SUM(monto)>'5000000';
+
+
+-- CREACIÓN DE VISTAS
+CREATE VIEW VW_KV_INFO_PRESTAMOS AS
+SELECT 
+	p.id AS 'prestamo_id',
+	p.tipo_prestamo_id,
+	tp.nombre AS 'tipo_prestamo',
+	p.monto, 
+	p.tasa_interes,
+	p.plazo_meses 
+FROM prestamos p, tipos_prestamo tp
+WHERE 
+	p.tipo_prestamo_id=tp.id AND 
+	tp.nombre NOT IN ('Préstamo Personal','Préstamo Educativo','Línea de Crédito');
+
+	SELECT*FROM VW_KV_INFO_PRESTAMOS;
+
+	DROP VIEW VW_KV_INFO_PRESTAMOS;
