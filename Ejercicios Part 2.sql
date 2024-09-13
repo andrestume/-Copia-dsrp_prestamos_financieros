7a7e391ebd7dea4b72b036763db8efe8f0c26193
Kevin Rivera <kevinriveravergaray@gmail.com>
2024-09-12 21:46:03 -0500

    ejercicos part 2

---
+++ Ejercicios Part 2.sql
@@ -0,0 +1,30 @@
+/*Ejercicio 1: Listar Clientes con Préstamos Activos
+Obtén una lista de todos los clientes que tienen préstamos activos. Muestra el nombre completo del cliente, el número de documento y el monto total del préstamo.
+
+Ejercicio 2: Cuotas Pendientes por Cliente
+Genera un informe que muestre el número de cuotas pendientes por cliente. Incluye el nombre completo del cliente, el número de documento y el número de cuotas pendientes.
+
+Ejercicio 3: Total Pagado por Cliente
+Calcula el total pagado por cada cliente hasta la fecha. Muestra el nombre completo del cliente y el monto total pagado.
+
+Ejercicio 4: Préstamos con Pagos Parciales
+Encuentra los préstamos que tienen al menos una cuota con un pago parcial. Muestra el ID del préstamo, el nombre completo del cliente, la fecha de inicio del préstamo y el monto restante.
+
+Ejercicio 5: Empleados con más Préstamos Asignados
+Determina cuáles son los empleados que han gestionado más préstamos como oficiales de crédito. Muestra el nombre del empleado y el número total de préstamos asignados.
+
+Ejercicio 6: Préstamos por Sucursal
+Muestra el total de préstamos activos en cada sucursal. Incluye el nombre de la sucursal y la suma de los montos de los préstamos.
+
+Ejercicio 7: Cuotas Vencidas por Préstamo
+Obtén una lista de todos los préstamos que tienen cuotas vencidas. Muestra el ID del préstamo, el nombre del cliente, la fecha de inicio del préstamo, el número de cuotas vencidas y el monto total vencido.
+
+Ejercicio 8: Métodos de Pago Utilizados
+Genera un informe que muestre cuántas veces se ha utilizado cada método de pago para realizar pagos. Muestra el nombre del método de pago y la cantidad de veces que se ha utilizado.
+
+Ejercicio 9: Renegociaciones de Préstamos
+Obtén un informe de todos los préstamos que han sido renegociados, incluyendo el ID del préstamo, el nombre del cliente y la nueva tasa de interés o plazo.
+
+Ejercicio 10: Préstamos por Tipo
+Muestra el número total de préstamos por cada tipo de préstamo. Incluye el nombre del tipo de préstamo y el número total de préstamos.
+*/
